9581b9f24c7f99f7e45b27ca09a2af2dc803d92b
Adrian Hernandez <131229205+dev66Ops@users.noreply.github.com>
2024-08-12 15:44:31 -0600

    first commit

---
+++ src/test/java/org/gestioncontratos/ContratosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gestioncontratos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContratosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
